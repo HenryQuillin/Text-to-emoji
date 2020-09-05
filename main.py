83c08b64d077603ee6698a78838ad6a5b0a4d7d5
HenryQuillin <henryquillin@gmail.com>
2020-09-04 20:57:17 -0500

    Moved script into a function

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-message = input(">")
-words = message.split(' ')
-emojis = {
-    ":)":"😊",
-    ":(":"😔"
-}
-output = ''
-for word in words:
-    output += emojis.get(word, word) + ' '
-print(output)
+input = input("Enter a message with ':(' or ':)' ")
+
+
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)":"😊",
+        ":(":"😔"
+    }
+    output = ''
+    for word in words:
+        output += emojis.get(word, word) + ' '
+    return output
+
+
+print(emoji_converter(input))
